b95de286614bfe2d03262c221e4ac22bba488a8f
shroog <shroogalhwity@gmail.com>
2021-10-13 10:30:44 +0300

    add pro2

--- ToDd.playground/Contents.swift
+++ ToDd.playground/Contents.swift
@@ -8,7 +8,7 @@ struct Item {
     var day: String
     var title:String
     var details:String
-    
+    var time :String
 }
 class ToDo {
     var tasks : [Item] = []
@@ -38,10 +38,12 @@ class ToDo {
           }
       }
 }
-var myList = ToDo(task: Item(day: "sunday", title: "appointment", details: "teeth appointment"))
-myList.add(addItem: Item(day: "sanday", title: "go to coffee", details: "meeting friend"))
+var myList = ToDo(task: Item(day: "sunday", title: "appointment", details: "teeth appointment" ,time: "05:00" ))
+myList.add(addItem: Item(day: "sanday", title: "go to coffee", details: "meeting friend" , time: "06:00"))
+myList.add(addItem: Item(day: "friday", title: "go to shopping Center", details: "buy clothes" ,time: "04:00"))
+myList.add(addItem: Item(day: "tuesday", title: "go to super market", details: "to buy food" ,time: "09:00"))
 myList.remove(remove: 10)
-myList.update(index: 0, update: Item(day: "monday", title: "go to coffee", details:"meeting friend" ))
+myList.update(index: 0, update: Item(day: "monday", title: "go to coffee", details:"meeting friend" ,time: "10:00" ))
 myList.read()
 
 [1,3,2,4,6].count
